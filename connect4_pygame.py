7f94fad69220026d8b85d11343e49ca098ba3449
AnawatPaotthong <47717997+AnawatPaotthong@users.noreply.github.com>
2020-11-21 00:17:06 +0700

    [FIX] connect_AI.py for draw condition and [ADD] test_font.py

--- connect4_pygame.py
+++ connect4_pygame.py
@@ -104,7 +104,7 @@ screen = pygame.display.set_mode(size)
 draw_board(board)
 pygame.display.update()
 
-myfont = pygame.font.SysFont("monospace", 50)
+myfont = pygame.font.SysFont("mvboli", 50)
 
 while not game_over:
 	
